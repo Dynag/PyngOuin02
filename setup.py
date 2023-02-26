62062b2fb28616cd6d6384ce28975edf7aab5b00
dynag <mgandy@ik.me>
2023-02-26 10:07:21 +0100

    1.3.1 Ajout d'une liste d'attente pour les envois automatiques

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='PyngOuin',
+    version='1.3.1',
+    packages=['fichier', 'fichier.lib', 'fichier.lib.idna', 'fichier.lib.email', 'fichier.lib.email.mime',
+              'fichier.lib.certifi', 'fichier.lib.urllib3', 'fichier.lib.urllib3.util', 'fichier.lib.urllib3.contrib',
+              'fichier.lib.urllib3.contrib._securetransport', 'fichier.lib.urllib3.packages',
+              'fichier.lib.urllib3.packages.backports', 'fichier.lib.requests', 'fichier.lib.connector',
+              'fichier.lib.connector.django', 'fichier.lib.connector.locales', 'fichier.lib.connector.locales.eng',
+              'fichier.lib.packaging', 'fichier.lib.pythonping', 'fichier.lib.charset_normalizer',
+              'fichier.lib.charset_normalizer.cli', 'fichier.lib.charset_normalizer.assets', 'fichier.Snyf'],
+    url='https://prog.hemge.eu',
+    license='',
+    author='Hemge',
+    author_email='',
+    description=''
+)
